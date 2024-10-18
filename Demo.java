aaa3344d71b7d12584d3dd0627d788cef6a595c5
Sachin <sachinm2460@gmail.com>
2024-10-18 10:39:01 +0530

    last changes

--- Demo.java
+++ Demo.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class Demo
 {
 	System.out.println("How are you");
@@ -9,4 +9,4 @@ class Demo
 {
   System.out.println("How are you");
 }
->>>>>>> c0e45d9e6a3863565ebcca30de5af938bc73e80e
+
